e471a5d7f1fb2d0f28bd97bda53d90ee8a1c9347
Tom Everett <tom@khubla.com>
2020-11-01 22:44:05 -0700

    gv suff

---
+++ src/main/java/com/khubla/dot4j/graphvis/ArrowShape.java
@@ -0,0 +1,13 @@
+package com.khubla.dot4j.graphvis;
+
+/**
+ * property name "shape" on a edge
+ * <p>
+ * http://www.graphviz.org/doc/info/arrows.html
+ * </p>
+ * 
+ * @author tom
+ */
+public enum ArrowShape {
+	box, crow, curve, icurve, diamond, dot, inv, none, normal, tee, vee
+}
---
+++ src/main/java/com/khubla/dot4j/graphvis/NodeShape.java
@@ -0,0 +1,13 @@
+package com.khubla.dot4j.graphvis;
+
+/**
+ * property name "shape" on a node
+ * <p>
+ * http://www.graphviz.org/doc/info/shapes.html
+ * </p>
+ * 
+ * @author tom
+ */
+public enum NodeShape {
+	box, polygon, ellipse, oval, circle, point, egg, triangle, plaintext, plain, diamond, trapezium, parallelogram, house, pentagon, hexagon, septagon, octagon, doublecircle, doubleoctagon, tripleoctagon, invtrianglem, invtrapezium, invhouse, Mdiamond, Msquare, Mcircle, rect, rectangle, square, star, none, underline, cylinder, note, tab, folder, box3d, component, promoter, cds, terminator, utr, primersite, restrictionsite, fivepoverhang, threepoverhang, noverhang, assembly, signature, insulator, ribosite, rnastab, proteasesite, proteinstab, rpromoter, rarrow, larrow, lpromoter
+}
